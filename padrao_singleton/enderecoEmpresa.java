e155a37d36650071377a56713cb6d8c7a37df2b8
Renan Magalhães Farias <115056464+renanmafarias@users.noreply.github.com>
2023-09-22 16:38:37 -0300

    Padrão Singleton atualizado

---
+++ padrao_singleton/enderecoEmpresa.java
@@ -0,0 +1,74 @@
+package padrao_singleton;
+
+public class EnderecoEmpresa {
+
+    private EnderecoEmpresa() {};
+    private static EnderecoEmpresa instance = new EnderecoEmpresa();
+    public static EnderecoEmpresa getInstance() {
+        return instance;
+    }
+
+    private String tipoLogradouro;
+    private String logradouro;
+    private int numero;
+    private String bairro;
+    private String municipio;
+    private String estado;
+    private int cep;
+
+    public String getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(String tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+}
